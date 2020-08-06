dda710a0c18b031ce63905c1f89fc66c03193a57
senadakadric <skadric@fordham.edu>
2020-08-06 12:45:44 -0400

    updated seeds and bookclubs controller for show page

--- app/controllers/bookclubs_controller.rb
+++ app/controllers/bookclubs_controller.rb
@@ -6,5 +6,16 @@ class BookclubsController < ApplicationController
         render json: bookclubs
     end
 
+    def show
+        bookclub = Bookclub.find(params[:id])
+
+        render json: bookclub
+    end
+
+    private
+
+    def bookclub_params
+        params.permit(:id)
+    end
 
 end
