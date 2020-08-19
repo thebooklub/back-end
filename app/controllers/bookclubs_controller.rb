4c91986130ec71bf2ecb233404ce7635df12de5a
senadakadric <skadric@fordham.edu>
2020-08-19 19:22:04 -0400

    fixed bug where book details not showing after joining a bookclub using a book from the api

--- app/controllers/bookclubs_controller.rb
+++ app/controllers/bookclubs_controller.rb
@@ -13,7 +13,9 @@ class BookclubsController < ApplicationController
     end
 
     def create
+        # byebug
         book = Book.find_or_create_by(id: bookclub_params[:book_id])
+        book.update(title: bookclub_params[:title], author: bookclub_params[:author], image_url: bookclub_params[:image_url])
         bookclub = Bookclub.find_or_create_by(book_id: book.id)
         bookclub.update(name: bookclub_params[:name])
         BookclubUser.find_or_create_by(bookclub_id: bookclub.id, user_id: @user.id)
@@ -24,7 +26,7 @@ class BookclubsController < ApplicationController
     private
 
     def bookclub_params
-        params.permit(:id, :book_id, :name, :bookclub)
+        params.permit(:id, :book_id, :name, :bookclub, :title, :author, :image_url)
     end
 
 end
